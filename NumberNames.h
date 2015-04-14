101b5ac0343e386045822fca7caed01069e2d1a6
Dev <dev.rangarajan@gmail.com>
2015-04-13 21:28:34 -0700

    Major Fixes, mostly works. Have to change a bit.
    Change prints to print eleven thousand and eleven. Also deal with
    "seventy two" cases.

--- NumberNames.h
+++ NumberNames.h
@@ -12,92 +12,40 @@ char *numbers[] = {"Zero ", "One ", "Two ", "Three ", "Four ", "Five ",
 				"Sixteen ", "Seventeen ", "Eighteen ", "Nineteen ", "Twenty ",
 				"Thirty ", "Fourty ", "Fifty ", "Sixty ", "Seventy ", "Eighty ",
 				"Ninety ", "Hundred ", "Thousand ", "Million"};
-//TODO ADD STORAGE FOR NUMBERS
 int mod_storage[7]; //Store remainders for next operation
 int num_storage[7]; //Store numbers for printing
-int pow_tens[] = {1, 10, 100, 1000, 10000, 100000, 1000000};
+int pow_tens[] = {1, 10, 100, 1000, 10000, 100000, 1000000};//Probably don't need
 
 //Functions
+int ilog(int n)
+{
+    return (int) log10f(n);
+}
+
+int getPlace(int n)
+{
+    return floor(ilog(n));
+}
 int sieveGeneral(int n, int pv)
 {
-	if(pv % 10 == 0 || pv == 1)
+	if((pv % 10 == 0 || pv == 1) && (n >= pv))
 	{
-		if(n >= pv)
-		{
-            if(n % pv >= pow_tens[(int)log10f(pv)-((int)log10f(pv) ? 1 : 0)])
-            {
-                mod_storage[(int)log10f(pv)-((int)log10f(pv) ? 1 : 0)] = n % pv;
-			    return floor(n/pv);
-            }
-            else
-            {
-                mod_storage[(int)log10f(pv)-  1] = 0;
-                return floor(n/pv);
-            }
-		}
-		else
-		{
-			return 0;
-		}
+        if(pv == 1)
+        {
+            mod_storage[getPlace(n % pv)] = n;
+        }
+        else
+        {
+            mod_storage[getPlace(n % pv)] = n % pv;
+        }
+        return floor(n/pv);
 	}
 	else
 	{
-		printf("ERROR. INVALID INPUT TO sieveGeneral(): n = %i, pv = %i\n", n, pv);
+		//printf("ERROR. INVALID INPUT TO sieveGeneral(): n = %i, pv = %i\n", n, pv);
 		return 0;
 	}
 }
-
-int getListE1(int stored)
-{
-	switch(stored)
-	{
-		case 11:
-			return 11;
-		case 12:
-			return 12;
-		case 13:
-			return 13;
-		case 14:
-			return 14;
-		case 15:
-			return 15;
-		case 16:
-			return 16;
-		case 17:
-			return 17;
-		case 18:
-			return 18;
-		case 19:
-			return 19;
-	}
-}
-int getListE2(int stored)
-{
-	switch(stored)
-	{
-		case 1:
-			getListE1(stored);
-		case 2:
-			return 20;
-		case 3:
-			return 21;
-		case 4:
-		 	return 22;
-		case 5:
-			return 23;
-		case 6:
-			return 24;
-		case 7:
-			return 25;
-		case 8:
-			return 26;
-		case 9:
-			return 27;
-	}
-}
-
-
-
 
 //TODO ADD DISPLAY FUNCTION
 void printE6(int n)
@@ -111,16 +59,16 @@ void printE5(int n)
 {
 	if(n != 0)
 	{
-		printf("%s%s%s", numbers[n], numbers[28], numbers[29]);
+		printf("%s%s", numbers[n], numbers[28]);//No thousand
 	}
 }
 void printE4(int n)//Don't want to print "thousand" on this command, but on printE3
 {
 	if(n == 1 && *numbers[n+1] != 0)
 	{
-		printf("%s%s", numbers[getListE1(n)], numbers[29]);//I think it's n to getList
+		printf("%s%s", numbers[n], numbers[29]);//I think it's n to getList
 	}
-	printf("%s", numbers[n]);
+	printf("%s", numbers[n+18]);
 }
 void printE3(int n)
 {
@@ -159,24 +107,25 @@ void displayGeneral(int n)
 	{
 		num_storage[i] = sieveGeneral(mod_storage[i], powE(i));//Should assign all values to the proper arrays
 	}
-/*
+
 	switch((int) floor(log10f(n)))
 	{
 		case 6:
-			printE6(mod_storage[6]);
+			printE6(num_storage[6]);
 		case 5:
-			printE5(mod_storage[5]);
+			printE5(num_storage[5]);
 		case 4:
-			printE4(mod_storage[4]);
+			printE4(num_storage[4]);
 		case 3:
-			printE3(mod_storage[3]);
+			printE3(num_storage[3]);
 		case 2:
-			printE2(mod_storage[2]);
+			printE2(num_storage[2]);
 		case 1:
-			printE1(mod_storage[1]);
+			printE1(num_storage[1]);
 			break;
 	}
-    */
+
+
 }
 
 
